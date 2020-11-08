e2a66ebe6490d86afdfb7c91262e07d03112dc71
sagatto <sagato@live.com>
2020-11-07 23:33:32 -0800

    multiple projects

--- app.js
+++ app.js
@@ -31,7 +31,10 @@ const promptUser = () => {
 };
 
 const promptProject = portfolioData => {
-  portfolioData.projects = [];
+    // If there's no 'projects' array property, create one
+    if (!portfolioData.projects) {
+        portfolioData.projects = [];
+    }
   console.log(`
   =================
   Add a New Project
@@ -71,10 +74,19 @@ const promptProject = portfolioData => {
       message: 'Would you like to enter another project?',
       default: false
     }   
-  ]);
+  ])
+  .then(projectData => {
+    portfolioData.projects.push(projectData);
+    if (projectData.confirmAddProject) {
+      return promptProject(portfolioData);
+    } else {
+      return portfolioData;
+    }
+  });
 };
 
 promptUser()
-  .then(answers => console.log(answers))
   .then(promptProject)
-  .then(projectAnswers => console.log(projectAnswers));
+  .then(portfolioData => {
+    console.log(portfolioData);
+  });
