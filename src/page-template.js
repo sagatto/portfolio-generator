3ab4f6f743ec53f2e5cd11f4e0f033f1139861b7
sagatto <sagato@live.com>
2020-11-08 20:00:50 -0800

    mockdata

--- src/page-template.js
+++ src/page-template.js
@@ -1,20 +1,25 @@
-const generatePage = (name, github) => {
-    return `
-      <!DOCTYPE html> 
-      <html lang="en"> 
-      <head>
-          <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <meta http-equiv="X-UA-Compatible" content="ie=edge">
-          <title>Portfolio Demo</title>
-      </head>
+module.exports = templateData => {
+    console.log(templateData);
+
+    // destructure projects and about data from templateData based on their property key names
+    const { projects, about } = templateData;
+
   
-      <body>
-          <h1>${name}</h1>
-          <h2><a href="https://github.com/${github}">Github</a></h2>
-      </body>
-      </html>
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+  
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Portfolio Demo</title>
+    </head>
+  
+    <body>
+      <h1>${templateData.name}</h1>
+      <h2><a href="https://github.com/${templateData.github}">Github</a></h2>
+    </body>
+    </html>
     `;
   };
-
-  module.exports = generatePage;
